1f731b5214b7ee2ca10c28cbbfa41961535bd160
DiamondStalker <cmoreno981@gmail.com>
2022-11-05 22:55:22 -0500

    dsadasdasdsa

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,56 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import axios from "axios";
+import Login from "./components/Login";
+import Weekdays from "./components/Weekdays"
+import Test from "./components/Test";
+import {motion} from 'framer-motion'
 
-//// 
 function App() {
-  const [count, setCount] = useState(0)
+  const urlWithProxy = "/api/obtenerusuarios";
+
+  const [displayLogin, setdisplayLogin] = useState("block");
+  const [displayDays, setDisplayDays] = useState("none");
+
+  useEffect(() => {
+    if (window.sessionStorage.getItem("Logged")) {
+      setdisplayLogin("none");
+      setDisplayDays("block");
+    } else {
+      console.log("No hay sesion");
+    }
+  }, []);
+
+  //console.log(displayLogin);
+
+
+  /*
+  function getDataFromServer() {
+    axios
+      .get(urlWithProxy)
+      .then((res) => console.log(res.data))
+      .catch((err) => {
+        console.error(err);
+      });
+  }
+*/
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+    
+      {/* ---------------------------------- LOGIN --------------------------------- */}
+      <div style={{ display: displayLogin }} className="login">
+        <Login />
+      </div>
+      {/* ---------------------------- Days of the week ---------------------------- */}
+      <motion.div className="daysOfWeek" style={{ display: displayDays }}
+      initial={{ opacity: 0, scale: 0.5 }}
+      animate={{ opacity: 1, scale: 1 }}
+      transition={{ duration: 0.5 }}
+      >
+        <Weekdays/>
+      </motion.div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
